0e81836cf3dd178d7b7bab65c729da80efac5566
DanteAlighierin <49035889+DanteAlighierin@users.noreply.github.com>
2020-10-08 17:20:01 +0300

    platform-independent directory, fullhd source

--- videoscrap.py
+++ videoscrap.py
@@ -1,9 +1,6 @@
 from pytube import YouTube
 link=input("enter you url = ")
 yt=YouTube(link)
-#t=yt.streams.filter(audio_codec='mp4a.40.2').all()
-#for i in t:
- #   print(t)
-video=yt.streams.get_by_itag(18)
-video.download(r'C:\Users\ADMIN\OneDrive\Desktop')
+video=yt.streams.get_by_itag(37)#fullhd resolution
+video.download('') # platform-independent directory
 print('done')
